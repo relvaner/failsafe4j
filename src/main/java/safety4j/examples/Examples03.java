f3ed716ea2df1fc370c51b17c77182e89b047553
relvaner <relvaner@gmail.com>
2016-11-28 18:26:10 +0100

    Update

--- src/main/java/safety4j/examples/Examples03.java
+++ src/main/java/safety4j/examples/Examples03.java
@@ -1,6 +1,6 @@
 /*
  * safety4j - Safety Library
- * Copyright (c) 2014, David A. Bauer
+ * Copyright (c) 2014-2016, David A. Bauer
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
